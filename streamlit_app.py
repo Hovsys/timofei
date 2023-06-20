c96414d0db69ed45a083c7d0a7b7d40d95de0a9b
Hovsys <107068591+Hovsys@users.noreply.github.com>
2023-06-21 00:01:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,11 @@ uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 # Флаг для отображения/скрытия изображения
 show_image = False
 
-# Отображение изображения после нажатия на кнопку "Show Image"
-if st.button('Show Image', key='show_image_button') and uploaded_file is not None:
+# Создание пустого контейнера для кнопки "Show Image"
+show_image_container = st.empty()
+
+# Обновление контейнера с помощью метода container.button()
+if show_image_container.button('Show Image', key='show_image_button') and uploaded_file is not None:
     show_image = True
 
 # Отображение изображения, если флаг установлен в True
@@ -16,10 +19,13 @@ if show_image:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
 
-# Кнопка для скрытия изображения
-if show_image and st.button('Hide Image', key='hide_image_button'):
-    show_image = False
+    # Кнопка для скрытия изображения
+    if st.button('Hide Image', key='hide_image_button'):
+        show_image = False
 
-# Скрытие кнопки "Show Image" после ее нажатия
-if show_image is False and uploaded_file is not None:
-    st.button('Show Image', key='show_image_button')
+    # Скрытие кнопки "Show Image", если изображение отображается
+    show_image_container.empty()
+
+# Показать кнопку "Show Image", если изображение не отображается
+if not show_image and uploaded_file is not None:
+    show_image_container.button('Show Image', key='show_image_button')
