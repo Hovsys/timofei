ad24c3064fc27b2cd2dfd99dc20d09981271f045
Hovsys <107068591+Hovsys@users.noreply.github.com>
2023-06-20 23:56:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,22 @@ from PIL import Image
 # Загрузка изображения
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
-# Переменная для отображения/скрытия изображения
+# Флаг для отображения/скрытия изображения
 show_image = False
 
 # Отображение изображения после нажатия на кнопку "Show Image"
-if st.button('Show Image'):
+if st.button('Show Image') and uploaded_file is not None:
     show_image = True
 
 # Отображение изображения, если флаг установлен в True
-if show_image and uploaded_file is not None:
+if show_image:
     image = Image.open(uploaded_file)
-    st.image(image, caption='', use_column_width=True)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
 
 # Кнопка для скрытия изображения
 if show_image and st.button('Hide Image'):
     show_image = False
+
+# Скрытие кнопки "Show Image" после ее нажатия
+if show_image is False and uploaded_file is not None:
+    st.button('Show Image')
