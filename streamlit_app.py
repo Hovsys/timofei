c7a2025ff11e27d81aca5cf44c44bb8e3d524840
Hovsys <107068591+Hovsys@users.noreply.github.com>
2023-06-20 23:27:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,24 +11,7 @@ def capture_image():
     stop_button = st.button('Сделать снимок', key='stop_capture', disabled=True)
     FRAME_WINDOW = st.image([])
 
-    # Открыть первую доступную камеру
-    camera = cv2.VideoCapture(0)
-    if not camera.isOpened():
-        st.error('Unable to Access Camera')
-        return
-
-    while start_button:
-        _, frame = camera.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        FRAME_WINDOW.image(frame)
-        if stop_button is not None:
-            stop_button.disabled = False
-        start_button = st.button('Включить камеру', key='run_capture', disabled=True)
-    camera.release()
-    if stop_button is not None:
-        stop_button.disabled = True
-    start_button = st.button('Включить камеру', key='run_capture', disabled=False)
-
+ 
 
 # Функция для загрузки нескольких изображений
 def upload_images():
@@ -46,7 +29,7 @@ def upload_images():
 
 
 # Создание веб-приложения
-st.title('ASL Recognition App')
+st.title('')
 option = st.sidebar.selectbox('Select an option', ('Capture an image', 'Convert images to English sentence'))
 
 if option == 'Capture an image':
