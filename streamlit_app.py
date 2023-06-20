dc7beb5b35455e8f13afe9681dedff6574669d2e
Hovsys <107068591+Hovsys@users.noreply.github.com>
2023-06-20 21:32:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,30 @@ import numpy as np
 import streamlit as st
 from PIL import Image
 
+# Функция для обработки изображения
+def preprocess_image(image, image_file, best_model, label_binarizer):
+    # Обработка изображения и предсказание буквы
+    # ...
+
+# Функция для захвата изображения с камеры
 def capture_image():
     st.subheader('Capture an image')
     FRAME_WINDOW = st.image([])
     camera = cv2.VideoCapture(0)
     while True:
         _, frame = camera.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         FRAME_WINDOW.image(frame)
         if st.button('Capture'):
             break
     camera.release()
+    # Обработка изображения и вывод результата
+    # Конвертация кадра в изображение Pillow
+    image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+    # Обработка изображения и вывод результата
+    letter = preprocess_image(np.array(image), None, best_model, label_binarizer)
+    st.write(f'The image is predicted as {letter}')
 
+# Функция для загрузки нескольких изображений
 def upload_images():
     st.subheader('Convert images to English sentence')
     sentence_image_files = st.file_uploader('Select the ASL Images', ['jpg', 'png'], accept_multiple_files=True)
@@ -28,6 +40,11 @@ def upload_images():
             sentence += letter
         st.write(f'The sentence is predicted as {sentence}')
 
+# Загрузка модели и данных
+best_model = None  # загрузите вашу модель здесь
+label_binarizer = None  # загрузите ваш объект LabelBinarizer здесь
+
+# Создание веб-приложения
 st.title('ASL Recognition App')
 option = st.sidebar.selectbox('Select an option', ('Capture an image', 'Convert images to English sentence'))
 
