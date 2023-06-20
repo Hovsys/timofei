68788a35a758d2ba6fbd42fb1fd4b395737af722
Hovsys <107068591+Hovsys@users.noreply.github.com>
2023-06-21 00:10:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 from PIL import Image
 
-# Загрузка изображения
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+# Загрузка изображений
+uploaded_files = st.sidebar.file_uploader("Choose images...", type="jpg", accept_multiple_files=True)
+
+# Список с загруженными изображениями
+images = []
+for uploaded_file in uploaded_files:
+    image = Image.open(uploaded_file)
+    images.append(image)
 
 # Флаг для отображения/скрытия изображения
 show_image = False
@@ -11,13 +17,22 @@ show_image = False
 show_image_container = st.empty()
 
 # Обновление контейнера с помощью метода container.button()
-if show_image_container.button('Show Image', key='show_image_button_' + str(show_image)) and uploaded_file is not None:
+if show_image_container.button('Show Image', key='show_image_button_' + str(show_image)) and len(images) > 0:
     show_image = True
+
+# Индекс текущего изображения
+current_image_index = 0
 
 # Отображение изображения, если флаг установлен в True
 if show_image:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(images[current_image_index], caption='Uploaded Image', use_column_width=True)
+
+    # Кнопки для переключения между изображениями
+    col1, col2, col3 = st.beta_columns(3)
+    if col2.button('Previous', key='previous_button'):
+        current_image_index = (current_image_index - 1) % len(images)
+    if col2.button('Next', key='next_button'):
+        current_image_index = (current_image_index + 1) % len(images)
 
     # Кнопка для скрытия изображения
     if st.button('Hide Image', key='hide_image_button_' + str(show_image)):
