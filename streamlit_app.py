5ff7f115b13cd3f6d077977f31f8bc8f2ec71212
Hovsys <107068591+Hovsys@users.noreply.github.com>
2023-06-20 23:44:36 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,18 @@
-import cv2
-import numpy as np
 import streamlit as st
 from PIL import Image
 
-# Функция для загрузки нескольких изображений
-def upload_images():
-    st.subheader('')
-    sentence_image_files = st.file_uploader('Select Images', ['jpg', 'png'], accept_multiple_files=True)
+# Загрузка изображения
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
-    if sentence_image_files is not None:
-        for i, image_file in enumerate(sentence_image_files):
-            st.subheader(f'Image {i+1}')
-            image = Image.open(image_file).convert('RGB')
-            st.image(image, use_column_width=True, caption=image_file.name)
-            if st.button(f"View/Hide Image {i+1}"):
-                st.image(image, use_column_width=True)
-                st.write(f'Image {i+1} is displayed')
-            else:
-                st.write(f'Image {i+1} is hidden')
+# Отображение изображения
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
 
-def capture_image():
-    st.subheader('Просмотр изображения')
-    start_button = st.button('Посмотреть изображение')
-    stop_button = st.button('Скрыть изображение', key='stop_capture', disabled=True)
-    FRAME_WINDOW = st.image([])
-
-# Создание веб-приложения
-st.title('')
-
+# Кнопки для отображения/скрытия изображения
+if st.button('Show Image'):
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+if st.button('Hide Image'):
+    st.empty()
    
 
