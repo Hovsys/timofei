1228b8844bd437d69f3abf379d3c14278c95b38c
Hovsys <107068591+Hovsys@users.noreply.github.com>
2023-06-20 23:37:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,53 +3,33 @@ import numpy as np
 import streamlit as st
 from PIL import Image
 
-
-# Функция для захвата изображения с камеры
-def capture_image():
-    st.subheader('Capture an image')
-    start_button = st.button('Включить камеру')
-    stop_button = st.button('Сделать снимок', key='stop_capture', disabled=True)
-    FRAME_WINDOW = st.image([])
-
-    # Открыть первую доступную камеру
-    camera = cv2.VideoCapture(0)
-    if not camera.isOpened():
-        st.error('Unable to Access Camera')
-        return
-
-    while start_button:
-        _, frame = camera.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        FRAME_WINDOW.image(frame)
-        if stop_button is not None:
-            stop_button.disabled = False
-        start_button = st.button('Включить камеру', key='run_capture', disabled=True)
-    camera.release()
-    if stop_button is not None:
-        stop_button.disabled = True
-    start_button = st.button('Включить камеру', key='run_capture', disabled=False)
-
-
 # Функция для загрузки нескольких изображений
 def upload_images():
-    st.subheader('Convert images to English sentence')
-    sentence_image_files = st.file_uploader('Select the ASL Images', ['jpg', 'png'], accept_multiple_files=True)
+    st.subheader('')
+    sentence_image_files = st.file_uploader('Select Images', ['jpg', 'png'], accept_multiple_files=True)
 
-    if len(sentence_image_files) > 0:
-        sentence = ''
-        for image_file in sentence_image_files:
-            image = Image.open(image_file).convert('L')
-            image = np.array(image, dtype='float32')
-            letter = preprocess_image(image, image_file, best_model, label_binarizer)
-            sentence += letter
-        st.write(f'The sentence is predicted as {sentence}')
+    if sentence_image_files is not None:
+        for i, image_file in enumerate(sentence_image_files):
+            st.subheader(f'Image {i+1}')
+            image = Image.open(image_file).convert('RGB')
+            st.image(image, use_column_width=True, caption=image_file.name)
+            if st.button(f"View/Hide Image {i+1}"):
+                st.image(image, use_column_width=True)
+                st.write(f'Image {i+1} is displayed')
+            else:
+                st.write(f'Image {i+1} is hidden')
 
+def capture_image():
+    st.subheader('Просмотр изображения')
+    start_button = st.button('Посмотреть изображение')
+    stop_button = st.button('Скрыть изображение', key='stop_capture', disabled=True)
+    FRAME_WINDOW = st.image([])
 
 # Создание веб-приложения
-st.title('ASL Recognition App')
-option = st.sidebar.selectbox('Select an option', ('Capture an image', 'Convert images to English sentence'))
+st.title('')
+option = st.sidebar.selectbox('Select an option', ('Просмотр изображения', 'Загрузка изображения'))
 
-if option == 'Capture an image':
+if option == 'Просмотр изображения':
     capture_image()
 else:
     upload_images()
