2c8e739d00c1ec023e08e48f37bb07d36d48f223
Hovsys <107068591+Hovsys@users.noreply.github.com>
2023-06-20 23:26:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,36 @@ import streamlit as st
 from PIL import Image
 
 
+# Функция для захвата изображения с камеры
 def capture_image():
-    st.subheader('1')
-    start_button = st.button('Посмотреть изображения')
-    stop_button = st.button('Скрыть изображения', key='stop_capture', disabled=True)
+    st.subheader('Capture an image')
+    start_button = st.button('Включить камеру')
+    stop_button = st.button('Сделать снимок', key='stop_capture', disabled=True)
     FRAME_WINDOW = st.image([])
 
- 
+    # Открыть первую доступную камеру
+    camera = cv2.VideoCapture(0)
+    if not camera.isOpened():
+        st.error('Unable to Access Camera')
+        return
+
+    while start_button:
+        _, frame = camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame)
+        if stop_button is not None:
+            stop_button.disabled = False
+        start_button = st.button('Включить камеру', key='run_capture', disabled=True)
+    camera.release()
+    if stop_button is not None:
+        stop_button.disabled = True
+    start_button = st.button('Включить камеру', key='run_capture', disabled=False)
+
 
 # Функция для загрузки нескольких изображений
 def upload_images():
-    st.subheader('2')
-
-    
-    sentence_image_files = st.file_uploader('Select Images', ['jpg', 'png'], accept_multiple_files=True)
+    st.subheader('Convert images to English sentence')
+    sentence_image_files = st.file_uploader('Select the ASL Images', ['jpg', 'png'], accept_multiple_files=True)
 
     if len(sentence_image_files) > 0:
         sentence = ''
@@ -30,8 +46,8 @@ def upload_images():
 
 
 # Создание веб-приложения
-st.title('')
-option = st.sidebar.selectbox('Select an option', ('1', '2'))
+st.title('ASL Recognition App')
+option = st.sidebar.selectbox('Select an option', ('Capture an image', 'Convert images to English sentence'))
 
 if option == 'Capture an image':
     capture_image()
