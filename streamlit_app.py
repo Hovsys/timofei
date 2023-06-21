982cb392976effddd6092e97f52760b2b6ff11d6
Hovsys <107068591+Hovsys@users.noreply.github.com>
2023-06-21 03:58:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,49 @@
 import streamlit as st
 from PIL import Image
  
-st.title('Режим фотографии')
+st.title('Photo Mode')
 
-# Загрузка изображений
+# Image upload
 uploaded_files = st.sidebar.file_uploader("Choose images...", type=["jpg", "png"], accept_multiple_files=True, key='image_uploader')
 
-# Список с загруженными изображениями
+# List of uploaded images
 images = []
 for uploaded_file in uploaded_files:
     image = Image.open(uploaded_file)
     images.append(image)
     
-# Флаг для отображения/скрытия изображения
+# Flag to show/hide image
 show_image = False
 
-# Создание пустого контейнера для кнопки "Show Image"
+# Create an empty container for the "Show Image" button
 show_image_container = st.empty()
 
-# Обновление контейнера с помощью метода container.button()
-if show_image_container.button('Включить камеру', key='show_image_button_' + str(show_image)) and len(images) > 0:
+# Update the container using the container.button() method
+if show_image_container.button('Turn On Camera', key='show_image_button_' + str(show_image)) and len(images) > 0:
     show_image = True
 
-# Индекс текущего изображения
+# Current image index
 current_image_index = 0
 
-# Отображение изображения, если флаг установлен в True
+# Display the image if the flag is set to True
 if show_image:
     st.image(images[current_image_index], use_column_width=True)
-    st.markdown('<p align="center">Сделайте фотографию жеста</p>', unsafe_allow_html=True)
+    st.markdown('<p align="center">Take a photo of the gesture</p>', unsafe_allow_html=True)
 
-    # Кнопки для переключения между изображениями
+    # Buttons to switch between images
     col1, col2, col3 = st.columns(3)
-    if col2.button('Сделать фото жеста', key='previous_button'):
+    if col2.button('Take Photo of Gesture', key='previous_button'):
         current_image_index = (current_image_index - 1) % len(images)
-    if col2.button('Следующий жест', key='next_button'):
+    if col2.button('Next Gesture', key='next_button'):
         current_image_index = (current_image_index + 1) % len(images)
 
-    # Кнопки для отмены последней буквы и выключения камеры
-    if col3.button('Удалить последнюю букву', key='undo_button'):
+    # Buttons to undo last letter and turn off camera
+    if col3.button('Undo Last Letter', key='undo_button'):
         st.write('Undo Last Letter Button Clicked')
-    if col3.button('Выключить режим фото', key='camera_button'):
+    if col3.button('Turn Off Photo Mode', key='camera_button'):
         st.write('Turn Off Camera Button Clicked')
 
-    st.markdown('Полученный перевод: abk')
+    st.markdown('The predicted translation is: abk')
    
-    # Скрытие кнопки "Show Image", если изображение отображается
+    # Hide the "Show Image" button if the image is displayed
     show_image_container.empty()
