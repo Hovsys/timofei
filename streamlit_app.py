6c8b9c323dbf35d7d11ee2abeb4e9e4cd8d0fc76
Hovsys <107068591+Hovsys@users.noreply.github.com>
2023-06-21 03:32:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ current_image_index = 0
 # Отображение изображения, если флаг установлен в True
 if show_image:
     st.image(images[current_image_index], use_column_width=True)
-    st.markdown('<p style="color: black;">Сделайте фотографию жеста</p>', unsafe_allow_html=True)
+    st.markdown('<p align="center">Сделайте фотографию жеста</p>', unsafe_allow_html=True)
 
     # Кнопки для переключения между изображениями
     col1, col2, col3 = st.columns(3)
