90c0c2497c23c26a62e330b2dedc6130fa30b934
Hovsys <107068591+Hovsys@users.noreply.github.com>
2023-06-21 03:26:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,12 @@ st.title('Режим фотографии')
 # Загрузка изображений
 uploaded_files = st.sidebar.file_uploader("Choose images...", type=["jpg", "png"], accept_multiple_files=True, key='image_uploader')
 
-
 # Список с загруженными изображениями
 images = []
 for uploaded_file in uploaded_files:
     image = Image.open(uploaded_file)
     images.append(image)
     
-
-
 # Флаг для отображения/скрытия изображения
 show_image = False
 
@@ -30,19 +27,19 @@ current_image_index = 0
 
 # Отображение изображения, если флаг установлен в True
 if show_image:
-    st.image(images[current_image_index], caption='Сделайте фотографию жеста', use_column_width=True)
+    st.image(images[current_image_index], caption='Сделайте фотографию жеста', use_column_width=True, caption_style={'color': 'black'})
 
     # Кнопки для переключения между изображениями
     col1, col2, col3 = st.columns(3)
     if col2.button('Сделать фото жеста', key='previous_button'):
         current_image_index = (current_image_index - 1) % len(images)
-    if col2.button('Следующий    жест', key='next_button'):
+    if col2.button('Следующий жест', key='next_button'):
         current_image_index = (current_image_index + 1) % len(images)
 
     # Кнопки для отмены последней буквы и выключения камеры
     if col3.button('Удалить последнюю букву', key='undo_button'):
         st.write('Undo Last Letter Button Clicked')
-    if col3.button('Выключить  режим  фото', key='camera_button'):
+    if col3.button('Выключить режим фото', key='camera_button'):
         st.write('Turn Off Camera Button Clicked')
 
     st.markdown('Полученный перевод: abk')
